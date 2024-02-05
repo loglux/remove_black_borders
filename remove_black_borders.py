de99fc29ec17e63ce1d6be4766b009f3e7a80009
loglux <call2vlad@outlook.com>
2024-02-05 22:20:46 +0000

    Enhance border detection and adjust sensitivity in image cropping script
    - Implement top and bottom border detection to fully automate the cropping of images from all sides, enhancing the script's utility for a wider range of images with varying border sizes.
    - Adjust the white threshold from 245 to 85 in the BorderAnalyzer class to improve the detection of lighter-colored borders, increasing the script's versatility and accuracy in processing images with less distinct borders.

--- remove_black_borders.py
+++ remove_black_borders.py
@@ -1,4 +1,3 @@
-# remove_black_borders.py is a Python script that removes black borders from scanned images.
 import cv2
 import os
 import numpy as np
@@ -8,27 +7,31 @@ from threading import Lock
 print_lock = Lock()
 
 class BorderAnalyzer:
-    def __init__(self, white_threshold=245):
+    def __init__(self, white_threshold=85):
         self.white_threshold = white_threshold
-
 
     def find_borders(self, img_path):
         img = cv2.imread(img_path)
         if img is None:
             print("Image not found.")
-            return None, None
-        # Convert to Grayscale in oder to simplify image processing
+            return None, None, None, None
+
+        # Convert to Grayscale to simplify image processing
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         h, w = gray.shape
 
         left_border = 0
         right_border = w - 1
+        top_border = 0
+        bottom_border = h - 1
+
         # Find left border
         for i in range(w):
             col = gray[:, i]
             if np.mean(col) > self.white_threshold:
                 left_border = i
                 break
+
         # Find right border
         for i in range(w - 1, -1, -1):
             col = gray[:, i]
@@ -36,25 +39,37 @@ class BorderAnalyzer:
                 right_border = i
                 break
 
-        return left_border, right_border
+        # Find top border
+        for i in range(h):
+            row = gray[i, :]
+            if np.mean(row) > self.white_threshold:
+                top_border = i
+                break
+
+        # Find bottom border
+        for i in range(h - 1, -1, -1):
+            row = gray[i, :]
+            if np.mean(row) > self.white_threshold:
+                bottom_border = i
+                break
+
+        return left_border, right_border, top_border, bottom_border
 
 class ImageCropper:
-    def crop_image(self, img_path, left_border, right_border, output_folder, target_width=None, target_height=None):
+    def crop_image(self, img_path, left_border, right_border, top_border, bottom_border, output_folder, target_width=None, target_height=None):
         img = cv2.imread(img_path)
         if img is None:
             with print_lock:
                 print("Image not found.")
             return
 
-        cropped_img = img[:, left_border:right_border]
-
+        cropped_img = img[top_border:bottom_border, left_border:right_border]
 
         if target_width and target_height:
             cropped_img = cv2.resize(cropped_img, (target_width, target_height))
 
         output_path = os.path.join(output_folder, os.path.basename(img_path))
         cv2.imwrite(output_path, cropped_img)
-
 
         with print_lock:
             if target_width and target_height:
@@ -77,10 +92,10 @@ def process_image(filename, input_folder, output_folder, target_width, target_he
         input_path = os.path.join(input_folder, filename)
         analyzer = BorderAnalyzer()
         cropper = ImageCropper()
-        left_border, right_border = analyzer.find_borders(input_path)
+        left_border, right_border, top_border, bottom_border = analyzer.find_borders(input_path)
 
-        if left_border is not None and right_border is not None:
-            cropper.crop_image(input_path, left_border, right_border, output_folder, target_width, target_height)
+        if left_border is not None and right_border is not None and top_border is not None and bottom_border is not None:
+            cropper.crop_image(input_path, left_border, right_border, top_border, bottom_border, output_folder, target_width, target_height)
 
 if __name__ == "__main__":
     input_folder = "book"  # Replace with your folder
